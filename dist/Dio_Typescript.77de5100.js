cb080b3e583e74a011616943ade5fbf103af9b57
Luis Fernando <luisff@dcomp.ufs.br>
2022-06-19 15:12:16 -0300

    criando arquivo index.ts e executando com parcel

--- dist/Dio_Typescript.77de5100.js
+++ dist/Dio_Typescript.77de5100.js
@@ -118,7 +118,16 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"index.ts":[function(require,module,exports) {
-console.log('TypeScript');
+function soma(a, b) {
+  return a + b;
+}
+
+console.log('TypeScript!');
+soma(10, 20);
+var animal = {
+  nome: 'camelo',
+  tipo: "terrestre"
+};
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +156,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57497" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52488" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log('TypeScript');
+function soma (a:number, b:number){
+    return a + b;
+}
+console.log('TypeScript!');
+soma( 10, 20)
+
+//types
+//interfaces
+interface IAnimal{
+    nome: string;
+    tipo: 'terrestre' | 'aquatico';
+}
+
+const animal: IAnimal = {
+    nome: 'camelo',
+    tipo: "terrestre" 
+}
